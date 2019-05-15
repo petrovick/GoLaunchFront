5b57aff5b353c666bd939d4f13a6edb7d7c5fda6
Gabriel Petrovick <petrovickg@hotmail.com>
2019-05-15 15:43:55 -0300

    Project removed for update

--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  //baseURL: 'https://immense-tor-58043.herokuapp.com',
-  baseURL: 'https://api.github.com',
-});
-
-export default api;
--- src/store/ducks/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-
-import favorites from "./favorites";
-import gamer from "./gamer";
-export default combineReducers({
-  favorites,
-  gamer
-});
