3df9b5447aefcb2ace53328037b466ec10991967
Gabriel Petrovick <petrovickg@hotmail.com>
2019-05-17 18:15:56 -0300

    HTTP para HTTPS

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './auth';
 
 const api = axios.create({
-  baseURL: 'http://immense-tor-58043.herokuapp.com',
+  baseURL: 'https://immense-tor-58043.herokuapp.com',
 });
 
 api.interceptors.request.use(async (config) => {
